80170e1ff1eb2b2a843d501f09646e50ed5c147b
RinLovesYou <xxklintan123xx@gmail.com>
2021-04-08 20:48:46 +0200

    Started

---
+++ PaperColor.cs
@@ -0,0 +1,8 @@
+namespace PPMLib
+{
+    public enum PaperColor
+    {
+        White = 1,
+        Black = 0
+    }
+}
---
+++ PenColor.cs
@@ -0,0 +1,9 @@
+namespace PPMLib
+{
+    public enum PenColor
+    {
+        Inverted = 1,
+        Red = 2,
+        Blue = 3
+    }
+}
