2a02c6acde43a64d48734a329e46e8ec0d3952ff
RinLovesYou <xxklintan123xx@gmail.com>
2021-04-08 21:00:54 +0200

    more stuff

---
+++ PPMFrame.cs
@@ -0,0 +1,11 @@
+using SixLabors.ImageSharp;
+namespace PPMLib
+{
+    public class PPMFrame
+    {
+        private PPMLayer _layer1 = new PPMLayer();
+        private PPMLayer _layer2 = new PPMLayer();
+        private PaperColor _paperColor;
+        private Image _frame = new Image()
+    }
+}
